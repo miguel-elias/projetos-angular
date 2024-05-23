7b70ed0e3a29624f22ba1806c126c4f4597870b1
Miguel Elias <elias.msalomao.miguel599@gmail.com>
2024-05-22 23:18:47 -0300

    add projeto users angular

---
+++ users-project/src/app/angular-material/angular-material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from "@angular/material/list";
+import { MatDividerModule } from '@angular/material/divider';
+
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatListModule,
+    MatDividerModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatListModule,
+    MatDividerModule
+  ],
+})
+export class AngularMaterialModule { }
