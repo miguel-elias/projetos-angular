ef16ab2da5f8fb9b432bff15ebab925817e9fe7c
Miguel Elias <elias.msalomao.miguel599@gmail.com>
2024-09-24 20:00:17 -0300

    add files and new features.

--- users-project/src/app/app.component.ts
+++ users-project/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
 import { UsersList } from './data/users-list';
 
@@ -7,10 +7,16 @@ import { UsersList } from './data/users-list';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  userslist: IUser[] = []; 
   userSelected: IUser = {} as IUser;
-
   showUserDetails: boolean = false;
+  
+  ngOnInit() {
+    setTimeout(() => {
+      this.userslist = UsersList;
+    }, 100);
+  }
 
   onUserSelected(user: IUser) {
     this.userSelected = user;
--- users-project/src/app/components/filter/filter.component.ts
+++ users-project/src/app/components/filter/filter.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IFilterOptions } from 'src/app/interfaces/filter/filter-options.interface';
 
 @Component({
   selector: 'app-filter',
@@ -6,11 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent {
-  foods = [
-    {
-      value: 'Ativo',
-      viewValue: 'Inativo',
-    }
-  ];
-
+filterOptions: IFilterOptions = {
+  name: undefined,
+  startDate: undefined,
+  endDate: undefined,
+  status: undefined,
+};
+statusList = [
+  {description: 'Ativo', value: true},
+  {description: 'Inativo', value: false}
+];
+onFilter() {
+  console.log(this.filterOptions);
+}
 }
